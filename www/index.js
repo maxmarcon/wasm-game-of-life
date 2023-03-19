8abab7d5477f0d3ffdf681065b5a091f4d802600
Max Marcon <maxmarcon@gmx.net>
2023-03-19 14:35:54 +0100

    less frequent renderings

--- www/index.js
+++ www/index.js
@@ -1,13 +1,18 @@
-import { Universe } from "wasm-game-of-life";
+import {Universe} from "wasm-game-of-life";
 
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
-const renderLoop = () => {
-    pre.textContent = universe.render();
-    universe.tick();
 
+let last_tick_ts;
+
+const renderLoop = (ts) => {
+    if (!last_tick_ts || ts - last_tick_ts >= 50) {
+        pre.textContent = universe.render();
+        universe.tick();
+        last_tick_ts = ts;
+    }
     requestAnimationFrame(renderLoop);
-};
+}
 
 requestAnimationFrame(renderLoop);
