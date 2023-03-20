c28e8c9f5172a37b3400d513abab7e22e3961c28
Max Marcon <maxmarcon@gmx.net>
2023-03-20 06:19:28 +0100

    canvas

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,18 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+    
     fn get_index(&self, row: u32, col: u32) -> usize {
         (row * self.width + col) as usize
     }
